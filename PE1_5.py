b25b72f46285d7aac1c9fb6c1e9d0370aa0176ca
mahnur26 <mahnur.rahman25@gmail.com>
2026-02-01 17:34:22 -0500

    Add files via upload

---
+++ PE1_5.py
@@ -0,0 +1,10 @@
+#Gas Mileage – At 23,352 miles (on the odometer) the tank is filled. At 23,695 miles the tank is filled again with th 11 gallons. How many miles per gallon did the car average between the two fillings?
+startingmiles=23352
+endingmiles=23695
+gallons=11
+distance=endingmiles-startingmiles
+averagemiles=distance/gallons
+print("distance traveled:",distance,"Miles")
+print("Gallon Used:",gallons,"Gallons")
+print("How many miles per gallon did the car average between two fillings?")
+print(round(averagemiles,3),"Miles/Gallon")

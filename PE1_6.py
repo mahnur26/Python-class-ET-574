17837713a04b7d908dbcab2113186faa31f4ec3b
mahnur26 <mahnur.rahman25@gmail.com>
2026-02-01 19:33:14 -0500

    Add files via upload

---
+++ PE1_6.py
@@ -0,0 +1,53 @@
+#1 - 12 evaluate the numeric expression without the computer, and then use Python to check your answer.
+#1) 2+3*4 
+print(2+3*4)
+#2) 1-7**2 
+print(1-7**2)
+#3) 1//2**3
+print(1//2**3)
+#4) (3+4)*5
+print((3+4)*5)
+#5) (5%3)*4
+print((5%3)*4)
+#6) (-2)**(-2)
+print((-2)**(-2))
+#7) 7//3
+print(7//3)
+#8) 14%4
+print(14%4)
+#9) 1+7%4
+print(1+7%4)
+#10) 14//4*4
+print(14//4*4)
+#11) 5//2+2
+print(5//2+2)
+#12) 5%5*5
+print(5%5*5)
+#13 – 18 determine whether the name is a valid variable name. Explain your answer.
+#13) NewYear.sales,not a valid variable name because it has dot and we cannot use that.
+#14) room&color,not a valid variable name because it has & which is a symbol.
+#15) TOrF_1040, it is a valid variable name beacuse it starts with a capital letter and also has small leters and numeric numbers.
+#16) 311HotLine 17) expense# 18) INCOME 101
+#16) 311HotLine,not a valid variable name because it starts with numbers and we cannot use numbers first.
+#17) expense#,not a valid variable name because it ended with # which is a comment is python programming.
+#18) INCOME 101,not a valid variable name because it has space in the middle.
+#19 – 24 rewrite the statements using augmented assignment operators
+#19) cost = cost + 5
+cost+=5
+#20) sum = sum * rate 
+sum*=rate
+#21) product = product / 10
+product/=10
+#22) cost = cost // num
+cost//=num
+#23) total = total – cost
+total-=cost
+#24) quotient = quotient % rate
+quotient%=rate
+#25 – 27 find the value of the function where a = 5 and b = 3
+#25) int(-a / b)
+x=int(-5/3)
+#26) round(a / b, 2)
+round(5/3,2)
+#27) abs(b - a)
+abs(3-5)
